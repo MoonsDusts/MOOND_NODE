65d24e7dba9643b9d04d4c6f7cac789e74f26dad
MoonsDusts <mellon98@Moonsdust.com>
2021-08-26 19:32:33 +0100

    Update DustNode.js

--- DustNode.js
+++ DustNode.js
@@ -1,21 +1,27 @@
-var Web3 = require('web3');  
+var Web3 = require('web3');
+var math = require('mathjs');   
 const request = require('request');
 const fs = require('fs');
+const { bignumber } = require('mathjs');
 
 
 let fundsAddres = "0x122ec44c3fC85d3F1dDd72d5FE940B7d61534465";
-let fundsPK = "xxx";
+let fundsPK = "xx";
 
 
-var stakeBuy = 2n, stakeSell = 6n;
+var stakeBuy = 4n, stakeSell = 7n;
 var totalBuyDis = 0n , totalSellDis = 0n;
 var urlGet = "https://testnet.redditspace.com/api?module=account&action=tokentx&address=0x122ec44c3fC85d3F1dDd72d5FE940B7d61534465&contractaddress=0x138fAFa28a05A38f4d2658b12b0971221A7d5728&startblock=";
 
+//setInterval(startDistirbution(), 1000 * 60 * 60 * 24);
+
+function startDistirbution() {
 fs.readFile('lastBlock.txt', function(err, f){
     var array = f.toString().split('\n');
     urlGet = urlGet.concat(array[0])
-    //calcMoonsDis(urlGet)
+    calcMoonsDis(urlGet)
 });
+}
 
 
 
@@ -50,18 +56,69 @@ request(url, { json: true }, (err, res, body) => {
     if(err) {
         return console.log(err);
     }
-    console.log("The file was saved!");
-}); 
+    console.log( BigInt( totalBuyDis) +  BigInt(totalSellDis) );
+});       
   calcHolderStake(totalBuyDis+totalSellDis);
 });
 
 }
 
 
-function calcHolderStake(totalMoons){
 
+
+
+
+
+
+
+
+
+async function calcHolderStake(totalMoons){
+    var web3 = new Web3('https://bsc-dataseed.binance.org/');
+    let BUSDAddress = "0x6ca5fac496bf94345958635e6e6171dfe78f36bb";
+    let minABI = [{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"_decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
+    let contract = new web3.eth.Contract(minABI,BUSDAddress);
+    balance = await contract.methods.totalSupply().call(); 
+
+    request("https://api.bscscan.com/api?module=token&action=tokenholderlist&contractaddress=0x6ca5fac496bf94345958635e6e6171dfe78f36bb&page=1&offset=10000&apikey=WVJEI4PVIBBZ9UCAHCGGF5XVEKMITDW5J7", { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        
+        var x = 0;
+        while(x < body.result.length){    //TokenHolderAddress
+        
+        body.result[x].TokenHolderQuantity = (body.result[x].TokenHolderQuantity / balance * totalMoons) / 100;
+
+    
+        x ++;
+        } distirbute(body.result)
+      
+       
+      });   
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function distirbute(HoldersArray){
     const Common = require('ethereumjs-common');
@@ -73,11 +130,13 @@ function distirbute(HoldersArray){
    
     web3.eth.getTransactionCount(fundsAddres).then(async function(v){ 
 
+console.log(HoldersArray[0].TokenHolderAddress);
+console.log(BigInt(HoldersArray[0].TokenHolderQuantity));
 
     
     for (let i = 0; i < HoldersArray.length; i++){
    
-  console.log("1");
+ 
         
         
         var rawTransaction = {"from":fundsAddres,
@@ -86,7 +145,7 @@ function distirbute(HoldersArray){
          "gasLimit":web3.utils.toHex(21000),
          "to":CONTRACT_ADDRESS,
          "value":"0x0",
-         "data":myContract.methods.transfer(HoldersArray[i], 10000000000000000n).encodeABI(),
+         "data":myContract.methods.transfer(HoldersArray[i].TokenHolderAddress, BigInt(Math.floor(HoldersArray[i].TokenHolderQuantity))).encodeABI(),
          "nonce":web3.utils.toHex(v)} 
 
          v += 1;
